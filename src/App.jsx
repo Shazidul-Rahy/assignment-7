4cadf2e4590fc437be355495afc4cecb4f076bee
Shazidul-Rahy <shazidulhasan39@gmail.com>
2023-09-29 20:59:18 +0600

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
+import '../src/header/Header'
+import Header from '../src/header/Header'
+import Blogs from './assets/blogs/Blogs'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-6xl'>Hello!</h1>
+
+    <Header></Header>
+    <div><Blogs></Blogs></div>
+      
     </>
   )
 }
---
+++ src/header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  
+
+    return (
+      <>
+        <h1 className="font-bold text-3xl text-center mt-12">Course Registration</h1>        
+      </>
+    )
+  }
+  
+  export default Header
